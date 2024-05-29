a708d557f505d87c6e4e68ace37147a1dcf1b0e3
halejotezz <halecatez@gmail.com>
2024-05-28 21:38:52 -0500

    Add/Repository initialization

---
+++ src/main/java/com/hact/market/persistence/entity/Product.java
@@ -0,0 +1,9 @@
+package com.hact.market.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Product {
+}
